24a73dfbe04f614cd51605f0146897168b199fa6
nikolaizombie1 <fabio.matos999@gmail.com>
2024-06-15 09:05:49 -0400

    Initial commit, add jellyfin stack without API keys.
    The API keys are in the form of secret files.

---
+++ setup.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p jellyfin/cache
+mkdir -p jellyfin/config
+mkdir -p traefik/config/certs
+mkdir -p sonarr/data
+mkdir -p sonarr/config
+mkdir -p jellyseerr/config
+mkdir -p prowlarr/config
+mkdir -p transmission/config
