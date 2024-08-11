c571f3ba23260c221fcdb8265d85cad9248909e8
nikolaizombie1 <fabio.matos999@gmail.com>
2024-08-11 09:40:16 +0000

    Add gpu support to jellyfin, add missing creation of acme.json

--- setup.bash
+++ setup.bash
@@ -3,9 +3,11 @@
 mkdir -p jellyfin/cache
 mkdir -p jellyfin/config
 mkdir -p traefik/config/certs
+touch -p traefik/config/certs/acme.json
+chmod 600 traefik/config/certs/acme.json 
 mkdir -p sonarr/data
 mkdir -p sonarr/config
-mkdir -p jellyseerr/config
 mkdir -p prowlarr/config
 mkdir -p transmission/config
 mkdir -p gluetun
+mkdir -p radarr/config
